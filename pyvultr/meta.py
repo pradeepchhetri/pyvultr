9af231af608647bf97976dd85c94a3baf624fdba
Pradeep Chhetri <pradeep.chhetri89@gmail.com>
2017-03-07 01:27:19 +0530

    Added setup.py

---
+++ pyvultr/meta.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, print_function, unicode_literals
+
+__name__ = 'pyvultr'
+__description__ = 'A Python Client for Vultr'
+__version__ = '0.0.1'
+__author__ = 'Pradeep Chhetri'
+__author_email__ = 'pradeep.chhetri89@gmail.com'
+__url__ = 'https://github.com/pradeepchhetri/pyvultr'
---
+++ setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+from pyvultr.meta import (__version__, __description__, __author__,
+                          __author_email__, __url__)
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+packages = [
+    'pyvultr',
+    'pyvultr.lib'
+]
+
+requires = open("requirements.txt").read().split()
+
+setup(
+    name='pyvultr',
+    version=__version__,
+    description=__description__,
+    author=__author__,
+    author_email=__author_email__,
+    url=__url__,
+    packages=packages,
+    package_data={'': ['LICENSE']},
+    package_dir={'pyvultr': 'pyvultr'},
+    include_package_data=True,
+    install_requires=requires,
+    license=open('LICENSE').read(),
+    zip_safe=False,
+    classifiers=(
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ),
+)
