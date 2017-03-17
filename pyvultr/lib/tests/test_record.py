b71a97baf4e442837acc5247ee847882079b4ed5
Pradeep Chhetri <pradeep.chhetri89@gmail.com>
2017-03-17 21:11:50 +0545

    Added some tests

--- pyvultr/lib/tests/test_record.py
+++ pyvultr/lib/tests/test_record.py
@@ -38,5 +38,81 @@ class TestRecord(BaseTest):
         self.assertEqual(self.record.data, "127.0.0.1")
         self.assertEqual(self.record.ttl, 300)
 
+    @responses.activate
+    def test_create(self):
+        data = self.load_from_file('record/single.json')
+
+        url = self.base_url + 'dns/create_record'
+        responses.add(responses.POST,
+                      url,
+                      body=data,
+                      status=200,
+                      content_type='application/json')
+
+        record = pyvultr.lib.Record(
+            domain_name="example.in",
+            name="vultr",
+            type="A",
+            data="192.168.100.1",
+            token=self.token
+        ).create()
+
+        self.assertEqual(responses.calls[0].request.url,
+                         self.base_url + "dns/create_record")
+        self.assertEqual(record.domain, "example.in")
+        self.assertEqual(record.name, "vultr")
+        self.assertEqual(record.type, "A")
+        self.assertEqual(record.data, "192.168.100.1")
+
+    @responses.activate
+    def test_delete(self):
+        data = self.load_from_file('record/single.json')
+
+        url = self.base_url + 'dns/delete_record'
+        responses.add(responses.POST,
+                      url,
+                      body=data,
+                      status=200,
+                      content_type='application/json')
+
+        record = pyvultr.lib.Record(
+            domain_name="example.in",
+            recordid=3586458,
+            token=self.token
+        ).delete()
+
+        self.assertEqual(responses.calls[0].request.url,
+                         self.base_url + "dns/delete_record")
+        self.assertEqual(record.domain, "example.in")
+        self.assertEqual(record.name, None)
+        self.assertEqual(record.type, None)
+        self.assertEqual(record.data, None)
+
+    @responses.activate
+    def test_update(self):
+        data = self.load_from_file('record/single.json')
+
+        url = self.base_url + 'dns/update_record'
+        responses.add(responses.POST,
+                      url,
+                      body=data,
+                      status=200,
+                      content_type='application/json')
+
+        record = pyvultr.lib.Record(
+            domain_name="example.in",
+            name="vultr",
+            type="A",
+            data="192.168.100.2",
+            token=self.token
+        ).update()
+
+        self.assertEqual(responses.calls[0].request.url,
+                         self.base_url + "dns/update_record")
+        self.assertEqual(record.domain, "example.in")
+        self.assertEqual(record.name, "vultr")
+        self.assertEqual(record.type, "A")
+        self.assertEqual(record.data, "192.168.100.2")
+
 if __name__ == '__main__':
     unittest.main()
